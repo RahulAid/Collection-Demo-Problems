51cc0997f2360c31e1b32dc0766347b6a9444ba2
RahulAid <raccess97@gmail.com>
2022-12-14 23:37:28 +0530

    Dictionary

---
+++ DemoClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class DemoClass
+{
+    public void DictionaryDemo()
+    {
+        Console.WriteLine("\n---------------Dictionary Demo-------------");
+
+        Dictionary<int, string> objDictionary = new Dictionary<int, string>();
+        objDictionary.Add(1, "Person1");
+        objDictionary.Add(2, "Person2");
+        objDictionary.Add(3, "Person3");
+
+        Console.WriteLine("\nAccess value using key(key=1): " + objDictionary[1]);
+
+        Console.WriteLine("\nIterating Dictionary: ");
+        foreach (var element in objDictionary)
+        {
+            Console.WriteLine($"Key = {element.Key} & Value = {element.Value}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Collection_Demo_Problems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Collection Demo Classes");
+            DemoClass objDemo = new DemoClass();
+            objDemo.DictionaryDemo();
+        }
+    }
+}
